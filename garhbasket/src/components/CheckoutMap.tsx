1d10364fb46bf15d5f0a8ea13d4560913057e556
Shivam gupta <guptashivam9161@gmail.com>
2026-07-18 12:34:05 +0530

    map updation

--- garhbasket/src/components/CheckoutMap.tsx
+++ garhbasket/src/components/CheckoutMap.tsx
@@ -1,3 +1,59 @@
+// 'use client'
+
+// import React from 'react'
+// import "leaflet/dist/leaflet.css"
+// import{MapContainer , Marker , TileLayer , useMap} from "react-leaflet"
+// import L, {LatLngExpression} from "leaflet"
+
+
+
+//  const markerIcon = new L.Icon({
+//         iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+//         iconSize: [40, 40],
+//         iconAnchor: [20, 40]
+//     })
+
+//     type props={
+//       position:[number,number],
+//       setPosition:(pos:[number,number])=>void
+//     }
+
+// function CheckoutMap({position,setPosition}:props) {
+//    const DraggableMarker = ({ position, setPosition }: { position: [number, number], setPosition: (p: [number, number]) => void }) => {
+//         return (
+//             <Marker
+//                 icon={markerIcon}
+//                 position={position}
+//                 draggable={true}
+//                 eventHandlers={{
+//                     dragend: (e: L.LeafletEvent) => {
+//                         const marker = e.target as L.Marker
+//                         const { lat, lng } = marker.getLatLng()
+//                         setPosition([lat, lng])
+//                     }
+//                 }}
+//             />
+//         )
+//     }
+//   return (
+//    <MapContainer
+//   center={position as LatLngExpression}
+//   zoom={13}
+//   scrollWheelZoom={true}
+//   className='w-full h-full'
+// >
+//   <TileLayer
+//     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//   />
+//   <DraggableMarker />
+// </MapContainer>
+//   )
+// }
+
+// export default CheckoutMap
+
+
 'use client'
 
 import React from 'react'
@@ -46,7 +102,7 @@ function CheckoutMap({position,setPosition}:props) {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <DraggableMarker />
+  <DraggableMarker position={position} setPosition={setPosition} />
 </MapContainer>
   )
 }
